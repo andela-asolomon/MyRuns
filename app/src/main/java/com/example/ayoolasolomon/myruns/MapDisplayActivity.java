12521ae95e52d6afff9789d59c925e41f8d63faa
Ayoola Solomon <ayoola.solomon@andela.co>
2015-08-20 15:08:05 +0100

    Adding Map

--- app/src/main/java/com/example/ayoolasolomon/myruns/MapDisplayActivity.java
+++ app/src/main/java/com/example/ayoolasolomon/myruns/MapDisplayActivity.java
@@ -2,8 +2,6 @@ package com.example.ayoolasolomon.myruns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -49,26 +47,4 @@ public class MapDisplayActivity extends AppCompatActivity {
 
     finish();
   }
-
-//  @Override
-//  public boolean onCreateOptionsMenu(Menu menu) {
-//    // Inflate the menu; this adds items to the action bar if it is present.
-//    getMenuInflater().inflate(R.menu.menu_map_display, menu);
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean onOptionsItemSelected(MenuItem item) {
-//    // Handle action bar item clicks here. The action bar will
-//    // automatically handle clicks on the Home/Up button, so long
-//    // as you specify a parent activity in AndroidManifest.xml.
-//    int id = item.getItemId();
-//
-//    //noinspection SimplifiableIfStatement
-//    if (id == R.id.action_settings) {
-//      return true;
-//    }
-//
-//    return super.onOptionsItemSelected(item);
-//  }
 }
